649891f287ee7402a0dafa3934b99f0dc072a71b
Luis Gustavo Santarosa Pinto <gustavo-computacao@hotmail.com>
2020-08-10 23:00:53 -0300

    Configurando Jwt Tokens
    Link https://jwt-auth.readthedocs.io/en/develop/laravel-installation/

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/* Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+}); */
+
+Route::post('auth/login', 'Api\\Pessoas\\AuthController@login');
+Route::post('auth/logout', 'Api\\Pessoas\\AuthController@logout');
+
+
+Route::group(['middleware' => ['apiJwt']], function () {
+    Route::get('users','Api\\Pessoas\\UserController@index');
 });
